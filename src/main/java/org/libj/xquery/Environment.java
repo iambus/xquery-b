f14cdf58319887bf23defd4050f2388358bf57e8
Boyu Guo <iambus@gmail.com>
2012-01-26 21:36:17 +0800

    refine performance test case output

---
+++ src/main/java/org/libj/xquery/Environment.java
@@ -0,0 +1,4 @@
+package org.libj.xquery;
+
+public class Environment {
+}
--- src/test/java/org/libj/xquery/Asserts.java
+++ src/test/java/org/libj/xquery/Asserts.java
@@ -1,5 +1,10 @@
 package org.libj.xquery;
 
+import org.libj.xquery.namespace.StaticFunction;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.libj.xquery.Compiler.eval;
@@ -39,9 +44,9 @@ public class Asserts {
         }
         long end = System.currentTimeMillis();
         long executionMillis = end - start;
-        logTime(script, executionMillis);
+        logTime(script, executionMillis, n);
         logSpeed(n, executionMillis);
-        assertTrue(String.format("Timeout: %d > %d (loop %d)", executionMillis, targetMillis, n), executionMillis <= targetMillis);
+        assertTrue(String.format("Timeout: %d > %d (loop %s)", executionMillis, targetMillis, number(n)), executionMillis <= targetMillis);
     }
 
     public static void assertRepeatedEvalPerSecond(String script, int n) {
@@ -55,9 +60,9 @@ public class Asserts {
         }
         long end = System.currentTimeMillis();
         long executionMillis = end - start;
-        logTime(script, executionMillis);
+        logTime(script, executionMillis, n);
         logSpeed(n, executionMillis);
-        assertTrue(String.format("Timeout: %d > %d (loop %d)", executionMillis, targetMillis, n), executionMillis < targetMillis);
+        assertTrue(String.format("Timeout: %d > %d (loop %s)", executionMillis, targetMillis, number(n)), executionMillis < targetMillis);
     }
 
     public static void assertRepeatedCompilePerSecond(String script, int n) {
@@ -65,12 +70,22 @@ public class Asserts {
     }
     private static void logTime(String script, long ms) {
         boolean multiline = script.indexOf('\n') == -1;
+        System.out.println("----------------------------------------");
         System.out.println("It takes "+ms+" ms to execute this script: "+(multiline?"":"\n")+script);
     }
-    private static void logSpeed(int n, long ms) {
+    private static void logTime(String script, long ms, long loop) {
+        boolean multiline = script.indexOf('\n') == -1;
+        System.out.println("----------------------------------------");
+        System.out.println(String.format("It takes %s ms to execute this script (loop %s): %s", ms, number(loop), (multiline?"":"\n"), script));
+    }
+    private static void logSpeed(long n, long ms) {
         long perCall = ms/n;
         long perSecond = 1000*n/ms;
-        System.out.println("Average excution time: "+perCall+ " ms");
-        System.out.println("Speed: "+perSecond+ " /s");
+        System.out.println("Average execution time: "+perCall+ " ms");
+        System.out.println("Speed: "+number(perSecond)+ " /s");
+    }
+    private static String number(long n) {
+        NumberFormat format = NumberFormat.getIntegerInstance(Locale.US);
+        return format.format(n);
     }
 }
