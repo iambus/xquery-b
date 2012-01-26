7b632dbc3d04712de0f25c66b8e22a4e0f4426f3
Boyu Guo <iambus@gmail.com>
2012-01-26 23:16:55 +0800

    remove duplicate code

--- src/test/java/org/libj/xquery/TestFreeVariables.java
+++ src/test/java/org/libj/xquery/TestFreeVariables.java
@@ -5,12 +5,14 @@ import org.junit.Test;
 import static org.libj.xquery.Asserts.*;
 
 public class TestFreeVariables {
-    @Test
-    public void testSimple() {
-        Environment env = new Environment();
+    private Environment env = new Environment();
+    public TestFreeVariables() {
         env.putVariable("i", 1);
         env.putVariable("j", 7);
         env.putVariable("s", "abc");
+    }
+    @Test
+    public void testSimple() {
         assertEval("$i", 1, env);
         assertEval("$j", 7, env);
         assertEval("$i + 3", 4, env);
@@ -21,10 +23,6 @@ public class TestFreeVariables {
     }
     @Test
     public void testOverride() {
-        Environment env = new Environment();
-        env.putVariable("i", 1);
-        env.putVariable("j", 7);
-        env.putVariable("s", "abc");
         assertEvalString("for $i in 1 to 10 return $i + $j", "8 9 10 11 12 13 14 15 16 17", env);
     }
 }
