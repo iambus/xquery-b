23ec727449e8bad4f3fc2285f68c3c033415c1fa
Boyu Guo <iambus@gmail.com>
2012-03-04 11:52:37 +0800

    remove unused import

--- src/main/java/org/libj/xquery/xml/XPathUtils.java
+++ src/main/java/org/libj/xquery/xml/XPathUtils.java
@@ -1,7 +1,5 @@
 package org.libj.xquery.xml;
 
-import com.sun.org.apache.xpath.internal.NodeSet;
-import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
