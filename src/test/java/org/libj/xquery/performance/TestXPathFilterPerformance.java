3d8db3a08cf546670aeacc7d3fa6d8b836046e9b
Boyu Guo <iambus@gmail.com>
2012-07-22 08:30:12 +0800

    test StringNamespaceXML xpath filter

--- src/test/java/org/libj/xquery/performance/TestXPathFilterPerformance.java
+++ src/test/java/org/libj/xquery/performance/TestXPathFilterPerformance.java
@@ -3,6 +3,7 @@ package org.libj.xquery.performance;
 import org.junit.Test;
 import org.libj.xquery.XQuery;
 import org.libj.xquery.xml.XML;
+import org.libj.xquery.xml.str.StringNamespaceXML;
 import org.libj.xquery.xml.str.StringXML;
 
 import static org.junit.Assert.assertTrue;
@@ -12,13 +13,23 @@ import static org.libj.xquery.Compile.compile;
 
 public class TestXPathFilterPerformance {
     @Test
-    public void testFilter() {
+    public void testStringXML() {
         String xquery = "let $fv1 := $arg\n" +
                         "let $x := $fv1/ID\n" +
                         "where $x = '2'\n" +
                         "return $fv1";
         XML x = new StringXML("<x><ID>1</ID></x>");
-        assertRepeatedEvalMillisVar(xquery, 1000*1000*3, 1000, "arg", x);
+        assertRepeatedEvalMillisVar(xquery, 1000 * 1000 * 3, 1000, "arg", x);
+    }
+
+    @Test
+    public void testStringNamespaceXML() {
+        String xquery = "let $fv1 := $arg\n" +
+                        "let $x := $fv1/ID\n" +
+                        "where $x = '2'\n" +
+                        "return $fv1";
+        XML x = new StringNamespaceXML("<x><ID>1</ID></x>");
+        assertRepeatedEvalMillisVar(xquery, 1000 * 1000 * 3, 1000, "arg", x);
     }
 
     public static void assertRepeatedEvalMillisVar(String script, int n, int targetMillis, String varName, Object var) {
